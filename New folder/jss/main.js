0cc9080851981f9dd8fe70c363655f9950a92575
Ramiz666 <49520893+Ramiz666@users.noreply.github.com>
2019-05-23 13:02:26 +0400

    Add files via upload

---
+++ New folder/jss/main.js
@@ -0,0 +1,21 @@
+// const names = prompt ("Ramiz");
+// console.log([names]);
+// 1
+
+// const max = Math.max (5,15,35,45,55);
+// console.log (max);  
+// 2
+
+
+
+// const min = Math.min (5,15,35,45,55);
+// console.log (min);  
+// 3
+
+
+// const max = Math.max (5,15,35,45,55);
+// const min = Math.min (5,15,35,45,55);
+// console.log (min);  
+// let sum= min + max
+// console.log(sum);
+// 4 END 
